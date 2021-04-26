4e42b7af13f0d0bf397e234e0e89624138e03e1b
Tina Li <tina.li@elev.ga.ntig.se>
2021-04-26 15:08:32 +0200

    ftgyujioo

--- adgfb/Program.cs
+++ adgfb/Program.cs
@@ -13,9 +13,9 @@ namespace fghbj
 
             Raylib.SetTargetFPS(60);
 
-            Rectangle block = new Rectangle(50, 50, 50, 50);
+            Rectangle block = new Rectangle(100, 50, 50, 50);
+            Rectangle worldMap = new Rectangle(-200, -200, 1000, 1000);
             Rectangle avatar = new Rectangle(400 - 20, 300 - 20, 40, 40);
-
             Camera2D camera = new Camera2D();
             camera.zoom = 1;
             //camera.target = new Vector2(400, 300);
@@ -45,12 +45,39 @@ namespace fghbj
                     camera.offset.Y += 3f;
                 }
 
+                if (avatar.x > 763)
+                {
+                    avatar.x = avatar.x - 3;
+                    camera.offset.X = camera.offset.X + 3;
+                }
+
+                if (avatar.x < -200)
+                {
+                    avatar.x = avatar.x + 3;
+                    camera.offset.X = camera.offset.X - 3;
+                }
+
+                if (avatar.y < -200)
+                {
+                    avatar.y = avatar.y + 3;
+                    camera.offset.Y = camera.offset.Y - 3;
+                }
+
+                if (avatar.y > 563)
+                {
+                    avatar.y = avatar.y - 3;
+                    camera.offset.Y = camera.offset.Y + 3;
+                }
+
+
+
 
                 // Drawing
                 Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.WHITE);
+                Raylib.ClearBackground(Color.BLACK);
 
                 Raylib.BeginMode2D(camera);
+                Raylib.DrawRectangleRec(worldMap, Color.WHITE);
                 Raylib.DrawRectangleRec(block, Color.RED);
                 Raylib.DrawRectangleRec(avatar, Color.ORANGE);
                 Raylib.EndMode2D();
