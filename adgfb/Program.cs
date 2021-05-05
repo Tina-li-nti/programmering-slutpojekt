f46e397039b83061d01e4cdab7d8e300d1568cea
Tina Li <tina.li@elev.ga.ntig.se>
2021-05-05 14:39:59 +0200

    fgyujio

--- adgfb/Program.cs
+++ adgfb/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Numerics;
 using Raylib_cs;
+using System.Collections.Generic;
+
 
 namespace fghbj
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
@@ -13,11 +16,21 @@ namespace fghbj
 
             Raylib.SetTargetFPS(60);
 
+
             Rectangle block = new Rectangle(100, 50, 50, 50);
             Rectangle worldMap = new Rectangle(-200, -200, 900, 700);
             Rectangle avatar = new Rectangle(400 - 20, 300 - 20, 40, 40);
-            Rectangle textBox = new Rectangle(-100, 50, 30, 80);
+            Rectangle textBox = new Rectangle(150, -55, 250, 50);
+
+            int MAX_INPUT_CHARS = 9;
+            // char name[MAX_INPUT_CHARS + 1] = "\0";      // NOTE: One extra space required for line ending char '\0'
+            int letterCount = 0;
+            List<char> name = new List<char>();
+
+            bool mouseOnText = false;
+
             Camera2D camera = new Camera2D();
+
             camera.zoom = 1;
             //camera.target = new Vector2(400, 300);
 
@@ -70,6 +83,7 @@ namespace fghbj
                     camera.offset.Y = camera.offset.Y + 3;
                 }
 
+
                 bool areOverlapping = Raylib.CheckCollisionRecs(block, avatar);
 
                 // Drawing
@@ -84,9 +98,46 @@ namespace fghbj
                 {
                     Raylib.DrawText("... oh hey kid, wait you are not supposed to be here", -180, -100, 30, Color.RED);
                     Raylib.DrawText("who are you?", -100, -50, 30, Color.RED);
-                    Raylib.DrawRectangle(textBox, Color.GRAY);
-                }
+                    Raylib.DrawRectangleRec(textBox, Color.GRAY);
+                    if (Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), textBox)) mouseOnText = true;
+                    else mouseOnText = false;
 
+                    //  if (mouseOnText)
+                    {
+                        // Set the window's cursor to the I-Beam
+                        // Raylib.SetMouseCursor(MOUSE_CURSOR_IBEAM);
+
+                        // Get char pressed (unicode character) on the queue
+                        int key = Raylib.GetCharPressed();
+
+                        // Check if more characters have been pressed on the same frame
+                        while (key > 0)
+                        {
+                            //NOTE: Only allow keys in range [32..125]
+                            if ((key >= 32) && (key <= 125) && (name.Count < MAX_INPUT_CHARS))
+                            {
+                                // name[letterCount] = (char)key;
+                                name.Add((char)key);
+                                //letterCount++;
+                            }
+
+                            key = Raylib.GetCharPressed();  // Check next character in the queue
+                        }
+
+                        if (Raylib.IsKeyPressed(KeyboardKey.KEY_BACKSPACE))
+                        {
+                            //letterCount--;
+                            if (name.Count > 0) name.RemoveAt(name.Count - 1);
+                            //name[letterCount] = '\0';
+                        }
+                    }
+                    //else Raylib.SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+
+
+                    string namestr = new string(name.ToArray());
+                    Raylib.DrawText(namestr, 170, -55, 40, Color.RED);
+
+                }
                 Raylib.EndMode2D();
 
                 Raylib.EndDrawing();
